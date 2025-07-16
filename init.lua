d7b0ecc9dafa2aaa546d3a0ff67607a1e97b4f32
ritananashi <deizu573@gmail.com>
2025-07-16 13:19:48 +0900

    add lsp

--- init.lua
+++ init.lua
@@ -716,6 +716,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'solargraph',
+        'rubocop',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
