76f87b7164508b93a1d2f3a2c728e0387afc90e9
ritananashi <deizu573@gmail.com>
2025-07-16 13:19:42 +0900

    fix

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -19,7 +19,7 @@ return {
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E78284' })
+        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#f4b8e4' })
         vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C890' })
         vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#8caaee' })
         vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#ef9f76' })
