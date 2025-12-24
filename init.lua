fa942b7a03f67e7016934892ed7768d88ec6be91
ritananashi <deizu573@gmail.com>
2025-12-24 18:28:54 +0900

    fix

--- init.lua
+++ init.lua
@@ -681,16 +681,6 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        vtsls = {
-          settings = {
-            typescript = {
-              tsserver = {
-                maxTsServerMemory = 1024,
-              },
-            },
-          },
-        },
-
         prismals = {},
 
         tailwindcss = {},
